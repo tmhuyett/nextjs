dc83f45e47bb3e61089f39263aeba7736aad4adc
tmhuyett <toddhuyett@gmail.com>
2020-11-07 03:10:29 -0500

    sdfd

--- components/Header.js
+++ components/Header.js
@@ -15,11 +15,11 @@ const Header = (props) => (
         <div><i></i>
             <div className="inner">
             
-                <h1>James River Deer Barriers</h1>
-                <p>a low-profile, long-term solution to nuisance deer</p>
+                <h1 className="grayscale">James River Deer Barriers</h1>
+                <p className="weCare">"we care"</p>
             </div>
         </div>
-        <nav>
+        <nav className="navigate">
             <ul>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
